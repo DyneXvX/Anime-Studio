b6a8fad9554e59111f85f957c3c41f1df6a71374
Justin Thoms <DyneXvX@outlook.com>
2020-07-17 16:37:04 -0400

    Store Procedure Template finished

---
+++ Anime Studio.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Anime_Studio.DataAccess.Data.Repository.IRepository;
+using Anime_Studio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anime_Studio.DataAccess.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
